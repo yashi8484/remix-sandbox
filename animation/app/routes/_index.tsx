a004469bf63ded98c036c29d18fc5cbf0723f281
yashi8484 <kobayashi@giftee.co>
2024-01-10 18:24:15 +0900

    Lottie Player で取れるイベントを検知してみる

--- animation/app/routes/_index.tsx
+++ animation/app/routes/_index.tsx
@@ -1,4 +1,4 @@
-import { Player } from "@lottiefiles/react-lottie-player";
+import { Player, PlayerEvent } from "@lottiefiles/react-lottie-player";
 import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
@@ -17,6 +17,16 @@ export default function Index() {
         // src="https://lottie.host/c404786e-2d84-4239-a092-5fa55366d5a7/DRPRrsgJH4.json"
         src="/animation-test.json" // public 配下がホスティングされるのは @remix-run/serve の仕様
         style={{ height: "300px", width: "300px", border: "1px solid #000" }}
+        onEvent={(event) => {
+          if (event === PlayerEvent.Error) {
+            console.log({ event });
+            console.log("lottie-player error event");
+          } else if (event === PlayerEvent.Load) {
+            console.log("lottie-player load event");
+          } else if (event === PlayerEvent.Play) {
+            console.log("lottie-player play event");
+          }
+        }}
       ></Player>
       <div>
         <span>This is test application.</span>
