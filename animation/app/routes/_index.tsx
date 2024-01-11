38708f4c04f9d2f0e869ee64f92508955283ab43
yashi8484 <kobayashi@giftee.co>
2024-01-11 16:33:15 +0900

    アニメーション内テキスト変更を検証

--- animation/app/routes/_index.tsx
+++ animation/app/routes/_index.tsx
@@ -1,5 +1,7 @@
 import { Player, PlayerEvent } from "@lottiefiles/react-lottie-player";
 import type { MetaFunction } from "@remix-run/node";
+import { AnimationItem } from "lottie-web";
+import { useEffect, useState } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,13 +11,26 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const [lottieRef, setLottieRef] = useState<AnimationItem>();
+
+  useEffect(() => {
+    // 本来 onEvent 内で load イベントをハンドリングしたいところだが、load イベント時には lottieRef = null となっているため適さない。
+    // 従って lottieRef state の変化(値のセット)をチェックし、 テキスト更新を行う。
+    if (lottieRef) {
+      console.log({ lottieRef });
+      const elements = lottieRef.renderer.elements;
+      console.log({ elements });
+      elements[0].updateDocumentData({ t: "health" });
+    }
+  }, [lottieRef]);
+
   return (
     <>
       <Player
         autoplay
         keepLastFrame
         // src="https://lottie.host/c404786e-2d84-4239-a092-5fa55366d5a7/DRPRrsgJH4.json"
-        src="/animation-test.json" // public 配下がホスティングされるのは @remix-run/serve の仕様
+        src="/text-animation-test.json" // public 配下がホスティングされるのは @remix-run/serve の仕様
         style={{ height: "300px", width: "300px", border: "1px solid #000" }}
         onEvent={(event) => {
           if (event === PlayerEvent.Error) {
@@ -27,6 +42,7 @@ export default function Index() {
             console.log("lottie-player play event");
           }
         }}
+        lottieRef={(l) => setLottieRef(l)}
       ></Player>
       <div>
         <span>This is test application.</span>
